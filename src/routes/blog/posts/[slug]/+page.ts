8f06cd7d1fde590c7baf326b6fc379beec480e2d
Silvian Gulica <gulica.sv@gmail.com>
2024-04-29 02:38:32 +0300

    quickfix: delete blog/posts/[slug]

--- src/routes/blog/posts/[slug]/+page.ts
+++
@@ -1,8 +0,0 @@
-// import type { PageLoad } from "./$types";
-// export const load: PageLoad = async ({params}) => {
-//     const slug = params.slug;
-
-//     const markdownPost = await import(
-//         "../../../../"
-//     )
-// }

609ae373b4d9ccb30899e0b3f198e1b1a1b0498d
Silvian Gulica <gulica.sv@gmail.com>
2024-04-29 17:52:33 +0300

    final

--- src/lib/components/MainHeader.svelte
+++ src/lib/components/MainHeader.svelte
@@ -5,30 +5,40 @@
   import IconLink from "$lib/components/IconLink.svelte";
   import DarkmodeButton from "$lib/components/DarkmodeButton.svelte";
 </script>
-
-<header
-  class="fixed z-10 top-0 inset-x-0 border-b"
->
-  <nav
-    class="flex items-center justify-between max-w-5xl mx-auto
-        px-4 h-24"
-  >
-    <a href="/">
-      <Logo class="w-20 h-20 dark:fill-white " />
-    </a>
-
-    <div class="flex space-x-1 items-center">
-      <IconLink
-        href="https://www.linkedin.com/in/silviangulica/"
-        target="_blank"
-      >
-        <LinkedIn class="w-7 h-8" />
-      </IconLink>
-
-      <IconLink href="https://github.com/silviangulica" target="_blank">
-        <Github class="w-7 h-8 " />
-      </IconLink>
+<!---->
+<!-- <header -->
+<!--   class="fixed z-20 top-0 inset-x-0 " -->
+<!-- > -->
+<!--   <nav -->
+<!--     class="flex items-center justify-between max-w-5xl mx-auto -->
+<!--         px-4 h-24" -->
+<!--   > -->
+<!--     <a href="/"> -->
+<!--       <Logo class="w-20 h-20 dark:fill-white " /> -->
+<!--     </a> -->
+<!---->
+<!--     <div class="flex space-x-1 items-center"> -->
+<!--       <IconLink -->
+<!--         href="https://www.linkedin.com/in/silviangulica/" -->
+<!--         target="_blank" -->
+<!--       > -->
+<!--         <LinkedIn class="w-7 h-8" /> -->
+<!--       </IconLink> -->
+<!---->
+<!--       <IconLink href="https://github.com/silviangulica" target="_blank"> -->
+<!--         <Github class="w-7 h-8 " /> -->
+<!--       </IconLink> -->
+<!--       <DarkmodeButton /> -->
+<!--     </div> -->
+<!--   </nav> -->
+<!-- </header> -->
+<div class="navbar bg-base-300">
+  <div class="flex-1">
+    <a class="btn btn-ghost text-xl">Developer</a>
+  </div>
+  <div class="flex-none">
+    <button class="btn btn-square btn-ghost">
       <DarkmodeButton />
-    </div>
-  </nav>
-</header>
+    </button>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,25 @@
   import profile from "$lib/assets/img/profile.jpg"
 </script>
 
-<div class="h-screen">
-  <div class="pt-48 max-w-4xl mx-auto flex-row space-x-6 justify-start">
+<div class="">
+  <div class="pt-[4vh] max-w-4xl mx-auto flex-row space-x-6 justify-start">
     <div class="flex justify-center">
       <img alt="silvian gulica" src={profile} class="w-48 h-48 object-top object-cover rounded-full" />
     </div>
     <div>
       <h1 class="text-4xl">Gulica Silvian</h1>
       <span class="text-lg">Hi! I am Silvian, a junior dev passionate about web development, game development, unix systems!</span>
+      <div class="pt-4">
+        <a class="btn btn-primary" href="https://www.linkedin.com/in/silviangulica/">
+          LinkedIn
+        </a>
+        <a class="btn btn-primary" href="https://github.com/silviangulica">
+          Github
+        </a>
+        <a class="btn btn-primary">
+          CV
+        </a>
+      </div>
     </div>
   </div>
 </div>

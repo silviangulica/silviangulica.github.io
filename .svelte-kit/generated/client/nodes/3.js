422697b5e7dd978e6ec3f453e1e13f76327f8dfb
Silvian Gulica <gulica.sv@gmail.com>
2024-06-24 13:01:25 +0300

    push

--- .svelte-kit/generated/client/nodes/3.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/blog/posts/[slug]/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/blog/posts/[slug]/+page.svelte";
--- src/routes/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import ThemeInitializer from "$lib/components/ThemeInitializer.svelte";
-  import "../app.css";
-  import MainHeader from "$lib/components/MainHeader.svelte";
-</script>
-
-  <MainHeader />
-  <slot />
